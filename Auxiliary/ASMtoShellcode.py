c52cd3a772ac70793f17ce41fec11e7e083ff0c2
b3mb4m <b3mb4m@gmail.com>
2015-06-17 21:28:49 +0300

    Create ASMtoShellcode.py

---
+++ Auxiliary/ASMtoShellcode.py
@@ -0,0 +1,59 @@
+class complier(object):
+	#Author : B3mB4m
+	#Greetz : Bomberman
+	#This script directly convert asm files to shellcode. 
+	#That's only pre-release will be append windows,nasm etc.. 
+	#Stay tuned ! 
+	
+	#Tested on : Ubuntu 14.04 
+	#Fıle type : ELF
+	#Versıon   : x86
+	
+	def __init__(self):
+		self.target = sys.argv[1]
+		self.command1 = "nasm -f elf %s" % sys.argv[1]
+		self.filename = sys.argv[1].split(".")[0]
+		self.temporaryfile = "%s.o" % self.filename
+		self.command2 = "ld -s -o %s %s.o" % (self.filename,self.filename)
+		self.shellcode = command2 = r'''for i in $(objdump -d %s |grep "^ " |cut -f2); do echo -n '\x'$i; done; echo''' % (self.filename)
+		self.getme = [self.command1, self.command2, self.shellcode]
+		self.complie()
+	
+	def complie(self):
+		if "NASM version" not in commands.getoutput("nasm -v"):
+			print "You must install nasm complier .."
+			print "apt-get install nasm" 
+			os.exit()
+
+		self.justincase()
+		coutme = 0
+		for i in self.getme:
+			commands.getoutput(i)
+			if coutme == 2:
+				if "\x00" in commands.getstatusoutput(i)[1]:
+					print "\n#WARNING : NULL BYTES HERE !#\n";
+					print "\n%s\n" % commands.getstatusoutput(i)[1]
+				else:
+					print "\n%s\n" % commands.getstatusoutput(i)[1]
+			coutme += 1	
+		self.justincase()	
+
+	def justincase(self):
+		if os.path.exists(self.filename) or os.path.exists(self.temporaryfile):
+			try:
+				os.remove(self.filename)
+				os.remove(self.temporaryfile)
+			except Exception as errorlog:
+				print errorlog.message 
+				os.exit()
+complier()
+
+
+/*
+There example;
+
+root@root:/home/b3mb4m/Desktop# python asmtoshellcode.py  XX.asm 
+
+\x31\xc0\x50\x68\x61\x64\x6f\x77\x68\x63 ....
+
+*/
