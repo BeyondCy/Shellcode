8c1ba598764c7a504b291ec4c9ede1fb61336782
b3mb4m <b3mb4m@gmail.com>
2015-09-13 12:40:25 +0300

    Create kernel32.c

---
+++ Windows/InlineC/kernel32.c
@@ -0,0 +1,27 @@
+#include <Windows.h>
+#include <stdio.h>
+
+//Coded By B3mB4m
+//gcc -masm=intel kernel32.c -o kernel.exe
+
+DWORD kernel( void )
+{
+	__asm__
+ 	(
+ 		".intel_syntax noprefix;"
+		"mov eax,[fs:0x30];" 				
+		"mov eax,[eax+0xc];"				
+		"mov eax,[eax+0x14];"			
+		"mov eax,[eax];"					
+		"mov eax,[eax];"					
+		"mov eax,[eax+0x10];"		
+	);
+
+}
+
+
+int main( void )
+{
+	printf("kernel32.dll : 0x%08X\n", kernel());
+}
+
