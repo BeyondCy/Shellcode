a48376f59a3247308ab38cf95f00b4a7772f6c4b
b3mb4m <b3mb4m@gmail.com>
2015-09-13 12:47:56 +0300

    Update kernel32.c

--- Windows/InlineC/kernel32.c
+++ Windows/InlineC/kernel32.c
@@ -4,17 +4,18 @@
 //Coded By B3mB4m
 //gcc -masm=intel kernel32.c -o kernel.exe
 
+
 DWORD kernel( void )
 {
 	__asm__
  	(
- 		".intel_syntax noprefix;"
+ 		".intel_syntax noprefix;"       
 		"mov eax,[fs:0x30];" 				
 		"mov eax,[eax+0xc];"				
 		"mov eax,[eax+0x14];"			
 		"mov eax,[eax];"					
 		"mov eax,[eax];"					
-		"mov eax,[eax+0x10];"		
+		"mov eax,[eax+0x10];"	
 	);
 
 }
@@ -23,5 +24,9 @@ DWORD kernel( void )
 int main( void )
 {
 	printf("kernel32.dll : 0x%08X\n", kernel());
+	//HMODULE lib = LoadLibrary("kernel32.dll");  Raw C codes
+	//printf("0x%p\n", lib);
 }
+
+
 
